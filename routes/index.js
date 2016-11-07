45634d604fbe0f5096a983a3ae04309d0a741ebe
vadimpirov85@gmail.com <vadimpirov85@gmail.com>
2016-11-07 16:41:29 +0300

    added api

--- routes/index.js
+++ routes/index.js
@@ -23,12 +23,43 @@ keystone.set('404', function(req, res, next) {
 // Load Routes
 var routes = {
     download: importRoutes('./download'),
+    api: importRoutes('./api'),
     views: importRoutes('./views'),
 };
 
 exports = module.exports = function(app) {
 
     app.all(middleware.theme);
+
+
+    // NOTE: To protect a route so that only admins can see it, use the requireUser middleware:
+    // app.get('/protected', middleware.requireUser, routes.views.protected);
+
+    // app.all('/api*', keystone.initCORS);
+
+
+    app.get('/api/post/list', [keystone.middleware.api, keystone.middleware.cors], routes.api.posts.list);
+    app.all('/api/post/create', [keystone.middleware.api, keystone.middleware.cors], routes.api.posts.create);
+    app.get('/api/post/:slug', [keystone.middleware.api, keystone.middleware.cors], routes.api.posts.get);
+    app.all('/api/post/:id/update', [keystone.middleware.api, keystone.middleware.cors], routes.api.posts.update);
+    app.get('/api/post/:id/remove', [keystone.middleware.api, keystone.middleware.cors], routes.api.posts.remove);
+
+    app.get('/api/post-category/list', [keystone.middleware.api, keystone.middleware.cors], routes.api.post_categories.list);
+    app.get('/api/post-category/:key', [keystone.middleware.api, keystone.middleware.cors], routes.api.post_categories.get);
+
+    app.get('/api/post-by-category/:key', [keystone.middleware.api, keystone.middleware.cors], routes.api.post_by_category.list);
+
+    app.get('/api/gallery/list', [keystone.middleware.api, keystone.middleware.cors], routes.api.galleries.list);
+    app.get('/api/gallery/:key', [keystone.middleware.api, keystone.middleware.cors], routes.api.galleries.get);
+
+    app.get('/api/enquiry/list', [keystone.middleware.api, keystone.middleware.cors], routes.api.enquiries.list);
+    app.all('/api/enquiry/create', [keystone.middleware.api, keystone.middleware.cors], routes.api.enquiries.create);
+    app.all('/api/enquiry/:id/update', [keystone.middleware.api, keystone.middleware.cors], routes.api.enquiries.update);
+    app.get('/api/enquiry/:id/remove', [keystone.middleware.api, keystone.middleware.cors], routes.api.enquiries.remove);
+
+    app.get('/api/enquiry_type/list', [keystone.middleware.api, keystone.middleware.cors], routes.api.enquiry_types.list);
+    app.get('/api/enquiry_type/:value', [keystone.middleware.api, keystone.middleware.cors], routes.api.enquiry_types.get);
+
 
     // Views
     app.get('/', routes.views.index);
