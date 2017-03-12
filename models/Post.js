71d07a6acd9e3be2e8f0cd5edebcf71960f66ce3
vadimpirov85@gmail.com <vadimpirov85@gmail.com>
2017-03-13 00:04:39 +0300

    added file to upload

--- models/Post.js
+++ models/Post.js
@@ -1,6 +1,7 @@
 var keystone = require('keystone');
 var ImageConverting = require('../services/image-converting');
 
+const postFileDir = './public/uploads/posts/files/';
 const postImgDir = './public/uploads/posts/img/';
 var postPreviewImgDir = postImgDir + '/preview/';
 
@@ -10,11 +11,32 @@ var Post = new keystone.List('Post', {
     autokey: {from: 'name', path: 'key', unique: true},
 });
 
-var myStorage = new keystone.Storage({
+var ImageStorage = new keystone.Storage({
     adapter: keystone.Storage.Adapters.FS,
+    schema: {
+        originalname: true
+    },
     fs: {
         path: keystone.expandPath(postImgDir), // required; path where the files should be stored
         publicPath: postImgDir // path where files will be served
+    }
+});
+
+// Add Option for Generated Filenames
+//https://github.com/keystonejs/keystone-storage-adapter-s3/pull/12/commits/9db761ae0d138d02b7b7c59fa7e59f761f5b95e8
+//https://github.com/keystonejs/keystone-storage-adapter-azure
+
+var FileStorage = new keystone.Storage({
+    adapter: keystone.Storage.Adapters.FS,
+    schema: {
+        originalname: true
+    },
+    fs: {
+        // generateFilename: function(a1, a2, a3, a4) {
+        //     return a1;
+        // },
+        path: keystone.expandPath(postFileDir), // required; path where the files should be stored
+        publicPath: postFileDir // path where files will be served
     }
 });
 
@@ -23,15 +45,29 @@ Post.add({
     state: {type: Types.Select, options: 'draft, published, archived', default: 'draft', index: true},
     author: {type: Types.Relationship, ref: 'User', index: true},
     publishedDate: {type: Types.Date, index: true},
-    image: {
-        type: Types.File, storage: myStorage
-    },
-    urls: { type: Types.TextArray },
-    urlsPodfm: { type: Types.TextArray },
     content: {
         brief: {type: Types.Html, wysiwyg: true, height: 150},
         extended: {type: Types.Html, wysiwyg: true, height: 400},
     },
+    image: {
+        type: Types.File, storage: ImageStorage
+    },
+
+    file0: {
+        type: Types.File, storage: FileStorage
+    },
+    file1: {
+        type: Types.File, storage: FileStorage
+    },
+    file2: {
+        type: Types.File, storage: FileStorage
+    },
+    file3: {
+        type: Types.File, storage: FileStorage
+    },
+
+    urls: { type: Types.TextArray },
+    urlsPodfm: { type: Types.TextArray },
     categories: {type: Types.Relationship, ref: 'PostCategory', many: true},
     tags: {type: Types.Relationship, ref: 'Tag', many: true},
     hits: {type: Types.Number, default: 0},
